2a73b2938c0a0667aafc135ec216ce5bb72e7192
Adam Casey <adam.jw.casey@gmail.com>
2023-01-08 12:00:53 -0700

    Passes test example

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,50 @@
 use std::collections::HashSet;
 
 #[derive(Debug)]
+#[derive(PartialEq, Eq, Hash)]
+struct Item{
+    val: char
+}
+
+impl Item{
+    fn new(item: char) -> Option<Self>{
+        match item.is_ascii_alphabetic(){
+            true => Some(Item{val: item}),
+            false => None
+        }
+    }
+
+    fn priority(&self) -> u32 {
+        match self.val.is_ascii_lowercase(){
+            true => self.val as u32 - 97 + 1,
+            false => self.val as u32 - 65 + 27
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Rucksack{
-    compartment1: String,
-    compartment2: String
+    compartment1: Vec<Item>,
+    compartment2: Vec<Item>
 }
 
 impl Rucksack{
     fn new(contents: &str) -> Option<Rucksack>{
         match contents.len() % 2 {
             0 => Some(Rucksack{
-                compartment1: contents[0..contents.len()/2].to_string(),
-                compartment2: contents[contents.len()/2..].to_string()
+                compartment1: contents[0..contents.len()/2].chars().map(|x| Item::new(x).unwrap()).collect(),
+                compartment2: contents[contents.len()/2..].chars().map(|x| Item::new(x).unwrap()).collect()
             }),
             1 => None,
             _ => panic!("Mod 2 (% 2) cannot return anything but 0 or 1"),
         }
+    }
+
+    fn duplicates(&self) -> HashSet<&Item>{
+        let set1: HashSet<&Item> = self.compartment1.iter().collect();
+        let set2: HashSet<&Item> = self.compartment2.iter().collect();
+
+        set1.intersection(&set2).map(|x| *x).collect()
     }
 }
 
@@ -30,6 +59,13 @@ impl Rucksack{
 /// ```
 pub fn calculate_priority(input: &String) -> u32 {
     let rucksacks = input.split('\n').filter(|x| !x.is_empty()).map(|x| Rucksack::new(x).unwrap());
-    dbg!(rucksacks.collect::<Vec<_>>());
-    10
+    rucksacks
+        .map(|rucksack| {
+            rucksack
+                .duplicates()
+                .iter()
+                .map(|item| item.priority())
+                .sum::<u32>()
+        })
+        .sum()
 }
