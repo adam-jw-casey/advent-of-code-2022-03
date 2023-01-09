9d6fcb16de7fa1d2eb3a5302e139480787768b14
Adam Casey <adam.jw.casey@gmail.com>
2023-01-08 17:35:05 -0700

    Passes test for part 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::collections::HashSet;
+use itertools::Itertools;
 
 #[derive(Debug)]
 #[derive(PartialEq, Eq, Hash)]
+#[derive(Clone)]
 struct Item{
     val: char
 }
@@ -48,6 +50,39 @@ impl Rucksack{
     }
 }
 
+impl  From<&Rucksack> for HashSet<Item>{
+    fn from(sack: &Rucksack) -> HashSet<Item> {
+        let set1: HashSet<Item> = sack.compartment1.iter().cloned().collect();
+        let set2: HashSet<Item> = sack.compartment2.iter().cloned().collect();
+
+        HashSet::from_iter(set1.union(&set2).map(|x| x.clone()))
+    }
+}
+
+struct Group{
+    rucksacks: [Rucksack; 3]
+}
+
+impl <'a> Group{
+    fn new<T: Iterator<Item = &'a str>>(raw: T) -> Self{
+        Self{
+            rucksacks: raw
+                .filter(|x| !x.is_empty())
+                .map(|x| Rucksack::new(x).unwrap())
+                .collect::<Vec<_>>().try_into().expect("There should be three rucksacks to unpack.")
+        }
+    }
+
+    fn find_badge(&self) -> Item{
+        let badge_set: HashSet<Item> = 
+            HashSet::from(&self.rucksacks[0])
+            .intersection(&HashSet::from(&self.rucksacks[1])).map(|x| x.clone()).collect::<HashSet<Item>>()
+            .intersection(&HashSet::from(&self.rucksacks[2])).map(|x| x.clone()).collect::<HashSet<Item>>();
+        assert_eq!(badge_set.len(), 1);
+        badge_set.iter().next().unwrap().clone()
+    }
+}
+
 /// Calculates the priority
 /// # Examples
 /// ```
@@ -67,5 +102,24 @@ pub fn calculate_priority(input: &String) -> u32 {
                 .map(|item| item.priority())
                 .sum::<u32>()
         })
+        .sum()
+}
+
+/// Finds the badges and calculates their priorities
+/// # Examples
+/// ```
+/// use std::fs;
+/// use advent_of_code_2022_03::calculate_badge_priority;
+///
+/// let contents = fs::read_to_string("example-input.txt").unwrap();
+/// assert_eq!(calculate_badge_priority(&contents), 70);
+/// ```
+pub fn calculate_badge_priority(input: &String) -> u32 {
+    input
+        .split('\n')
+        .filter(|x| !x.is_empty())
+        .into_iter().chunks(3).into_iter()
+        .map(|x| Group::new(x))
+        .map(|group| group.find_badge().priority())
         .sum()
 }
